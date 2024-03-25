2fe72b5c2d970ff1929beb11e0b90fa3868aa98d
William Wen <william@heyarctic.io>
2024-03-25 01:53:44 +0000

    fix: fixes to run on gcp vm
    Refs: NO-TICKET

--- scripts/vast/vision_captioner.sh
+++ scripts/vast/vision_captioner.sh
@@ -1,7 +1,7 @@
 python3 -m torch.distributed.launch \
 --nnodes 1 \
 --node_rank 0 \
---nproc_per_node 8 \
+--nproc_per_node 1 \
 --master_port 9814 \
 ./run.py \
 --config ./config/vast/captioner_cfg/caption-generation-vision.json \
@@ -10,4 +10,4 @@ python3 -m torch.distributed.launch \
 --test_batch_size 64 \
 --test_vision_sample_num 8 \
 --generate_nums 3 \
---captioner_mode true \
+--captioner_mode true
